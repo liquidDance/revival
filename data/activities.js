dd57740adda339bf662acf3de656949e507d982c
liquidDance <liquid.dance.revival@proton.me>
2025-09-05 01:07:05 +0200

    2oDeep

--- data/activities.js
+++
@@ -1,2 +0,0 @@
-// Este archivo contendrá los datos procesados de actividades
-// Se genera automáticamente a partir del CSV en script.js
--- data/participants.js
+++
@@ -1,7 +0,0 @@
-// Lista de participantes
-const allParticipants = [
-    "Aitor", "Alex", "Anabel", "Andreas", "Andrew", "Annick", 
-    "Anuja", "Blanca", "Claudia", "Diaana", "Dóri", "Francesco", 
-    "Germán", "Jessi", "Karenina", "Laura", "Marga", "Martín", 
-    "Nadia", "Natalia", "pareja", "Riki", "Tanja", "Willem"
-];
