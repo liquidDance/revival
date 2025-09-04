ce3e8b40e0b8895e0c034264dd1118bc3a3734fc
liquidDance <liquid.dance.revival@proton.me>
2025-09-04 23:07:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([38.993219, 1.563289], 15);
+let map = L.map('map').setView([38.993219, 1.563289], 10);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19
