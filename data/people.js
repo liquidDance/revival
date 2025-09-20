f34c70fdc7a56d27b9c2f6841d3cdeb149ce9811
liquidDance <liquid.dance.revival@proton.me>
2025-09-20 08:19:28 +0200

    granCambio

---
+++ data/people.js
@@ -0,0 +1,9 @@
+const allParticipants = [
+    "Aitor", "Alex", "Anabel", "Andreas", "Andrew", "Annick", 
+    "Anuja", "Blanca", "Claudia", "Diaana", "Dóri", "Francesco", 
+    "Germán", "Jessi", "Karenina", "Laura", "Marga", "Martín", 
+    "Nadia", "Natalia", "Pareja", "Riki", "Tanja", "Willem", "Zara", "Nati"
+];
+
+// Placeholder para futuros metadatos de participantes en cada foto
+window.photoPeople = {};
